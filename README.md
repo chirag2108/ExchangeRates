f7fceafc3f19c5eb6c3fe71458ad138646f0475a
chirag2108 <gchirag.it@gmail.com>
2019-04-01 15:46:19 +0530

    added build files

--- README.md
+++
@@ -1,6 +0,0 @@
-## please follow the below steps to run this project
-1. git clone https://github.com/chirag2108/ExchangeRates.git
-2. npm install
-3. npm start
-
-open http://localhost:3000 to view it in the browser
---
+++ precache-manifest.cbc6b08aa2f175d49db931001dd47bed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a7d097450708103405a",
+    "url": "/static/js/main.224d2b6e.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2a7d097450708103405a",
+    "url": "/static/css/main.ca9c2d9f.chunk.css"
+  },
+  {
+    "revision": "c9a70e74181f431999a7a1e9f6010b75",
+    "url": "/index.html"
+  }
+];
